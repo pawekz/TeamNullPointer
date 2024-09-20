9639b8af4b09f33f993aff959ccd206dc6f984a4
Paulo Carabuena <paulo.carabuena@cit.edu>
2024-09-20 12:55:13 +0800

    Added Modify (PUT) and Delete (DELETE) functionality, implementing CRUD

--- src/main/java/com/g2appdev/teamnullpointer/controller/AccountController.java
+++ src/main/java/com/g2appdev/teamnullpointer/controller/AccountController.java
@@ -12,18 +12,29 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
+    //test
     @GetMapping("/print")
     public String print() {
         return "Account Controller";
     }
-
+    //CREATE
+    @PostMapping("/postaccountrecord")
+    public AccountEntity postAccountRecord(@RequestBody AccountEntity account) {
+        return accountService.postAccountRecord(account);
+    }
+    //READ ALL
     @GetMapping("/getaccountrecords")
     public Iterable<AccountEntity> getAccountRecords() {
         return accountService.getAccountRecords();
     }
-
-    @PostMapping("/postaccountrecord")
-    public AccountEntity postAccountRecord(@RequestBody AccountEntity account) {
-        return accountService.postAccountRecord(account);
+    //UPDATE
+    @PutMapping("/updateaccountrecord")
+    public AccountEntity putAccountDetails(@RequestParam int id, @RequestBody AccountEntity newAccountDetails){
+        return accountService.putAccountDetails(id, newAccountDetails);
+    }
+    //DELETE
+    @DeleteMapping("/deleteaccountdetails/{id}")
+    public String deleteAccountDetails(@PathVariable int id) {
+        return accountService.deleteAccount(id);
     }
 }
