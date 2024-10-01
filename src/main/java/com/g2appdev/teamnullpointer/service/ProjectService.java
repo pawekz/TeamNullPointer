7e6c934c10993f231a207616052b24adf02262f7
Paulo Carabuena <paulo.carabuena@cit.edu>
2024-10-01 11:17:30 +0800

    Added Project (controller, entity, service), adjust gitignore

--- src/main/java/com/g2appdev/teamnullpointer/service/ProjectService.java
+++ src/main/java/com/g2appdev/teamnullpointer/service/ProjectService.java
@@ -5,7 +5,9 @@ import com.g2appdev.teamnullpointer.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.naming.NameNotFoundException;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class ProjectService {
@@ -20,7 +22,47 @@ public class ProjectService {
         return prepo.save(project);
     }
 
-    public List<ProjectEntity> getAllProjects(){
+    public List<ProjectEntity> getAllProject(){
         return prepo.findAll();
     }
+
+    public ProjectEntity postProjectRecord(ProjectEntity project) {
+        return prepo.save(project);
+    }
+
+    public List<ProjectEntity> getProjectRecords() {
+        return prepo.findAll();
+    }
+
+    @SuppressWarnings("finally")
+    public ProjectEntity putProjectDetails(int id, ProjectEntity newProjectDetails){
+        ProjectEntity project = new ProjectEntity();
+
+        try {
+            project = prepo.findById(id).get();
+
+            project.setPname(newProjectDetails.getPname());
+            project.setMembers(newProjectDetails.getMembers());
+            project.setInstructorId(newProjectDetails.getInstructorId());
+            project.setEndDate(newProjectDetails.getEndDate());
+
+        } catch (NoSuchElementException e) {
+            throw new NameNotFoundException("Project with id " + id + " not found");
+        }finally{
+            return prepo.save(project);
+        }
+    }
+
+    public String deleteProject(int id){
+        String msg = "";
+        if (prepo.findById(id) != null){
+            prepo.deleteById(id);
+            msg = "Successfully deleted the project";
+        }
+        else{
+            msg = "Project with id " + id + " not found";
+        }
+        return msg;
+    }
 }
+
